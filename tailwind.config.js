cdd804d5565a46fe252c1369acb3c31ac298b76b
Lautaro Garcia <lautarogonzalogarcia@outlook.com>
2023-06-13 17:01:04 -0300

    init wine cellars client
    taiwindCSS and tailwind-scrollbar instaled

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+
+    },
+    screens: {
+      'xsm': '320px',
+      'sm': '425px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1440px',
+      '3xl': '1920px',
+    }
+  },
+  plugins: [],
+}
+
